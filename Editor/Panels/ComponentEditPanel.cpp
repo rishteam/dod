2eeb2c25896f0631286017ba30739166d195dda7
roy4801 <a82611141@gmail.com>
2020-09-02 21:24:09 +0800

    [Editor] change edit widget

--- Editor/Panels/ComponentEditPanel.cpp
+++ Editor/Panels/ComponentEditPanel.cpp
@@ -7,12 +7,6 @@
 
 namespace rl {
 
-void ComponentEditPanel::onAttach(const Ref<Scene> &scene)
-{
-    SceneTargetPanel::onAttach(scene);
-    m_componentSelectionPanel.onAttach(scene);
-}
-
 template<>
 void ComponentEditPanel::drawEditComponentWidget<TagComponent>()
 {
@@ -39,12 +33,9 @@ void ComponentEditPanel::drawEditComponentWidget<TransformComponent>()
             return;
         //
         auto &transform = m_targetEntity.getComponent<TransformComponent>();
-        ImGui::SliderFloat("TranslateX", &transform.translate.x, -1.f, 1.f, "%.2f");
-        ImGui::SliderFloat("TranslateY", &transform.translate.y, -1.f, 1.f, "%.2f");
-        // ImGui::SliderFloat("Z", &transform.z, -10, 10, "%.2f");`x
+        ImGui::DragFloat3("Translate", glm::value_ptr(transform.translate), 0.01f);
         ImGui::Separator();
-        ImGui::DragFloat("ScaleX", &transform.scale.x, 0.1f, 0.f);
-        ImGui::DragFloat("ScaleY", &transform.scale.y, 0.1f, 0.f);
+        ImGui::DragFloat2("Scale", glm::value_ptr(transform.scale), 0.01f);
     }
 }
 
@@ -52,60 +43,65 @@ template<>
 void ComponentEditPanel::drawEditComponentWidget<RenderComponent>()
 {
     if (!m_targetEntity.hasComponent<RenderComponent>()) return;
-
+    //
     if (ImGui::CollapsingHeader("RenderComponent", ImGuiTreeNodeFlags_DefaultOpen))
     {
         if(drawEditComponentrightClickMenu<RenderComponent>())
             return;
         //
-        auto &renderCp = m_targetEntity.getComponent<RenderComponent>();
-        ImGui::ColorEdit4("Color", glm::value_ptr(renderCp.color));
+        auto &render = m_targetEntity.getComponent<RenderComponent>();
+        ImGui::ColorEdit4("Color", glm::value_ptr(render.color));
 
         ImGui::Text("Texture");
         {
             std::string tPath;
             if (ImGui::Button("Select")) {
                 if (FileDialog::SelectSingleFile(nullptr, nullptr, tPath)) {
-                    renderCp.texturePath = tPath;
-                    renderCp.reloadTexture = true;
+                    render.texturePath = tPath;
+                    render.reloadTexture = true;
                 }
             }
 
             ImGui::SameLine();
-            ImGui::InputText("##texturePath", const_cast<char *>(renderCp.texturePath.c_str()),
-                             renderCp.texturePath.size(), ImGuiInputTextFlags_ReadOnly);
+            ImGui::InputText("##texturePath", const_cast<char *>(render.texturePath.c_str()),
+                             render.texturePath.size(), ImGuiInputTextFlags_ReadOnly);
 
-            ImGui::Image(renderCp.m_texture->getTextureID(), ImVec2(64, 64), ImVec2(0, 0), ImVec2(1, -1));
+            ImGui::Image(render.m_texture->getTextureID(), ImVec2(64, 64), ImVec2(0, 0), ImVec2(1, -1));
         }
 
         ImGui::Text("Shader");
         {
             std::string path;
-            auto &vertPath = renderCp.vertPath;
+            auto &vertPath = render.vertPath;
             if (ImGui::Button("Select##Vert")) {
                 if (FileDialog::SelectSingleFile(nullptr, nullptr, path)) {
                     vertPath = path;
-                    renderCp.reloadShader = true;
+                    render.reloadShader = true;
                 }
             }
             ImGui::SameLine();
             ImGui::InputText("##VertexShaderPath", const_cast<char *>(vertPath.c_str()),
                              vertPath.size(), ImGuiInputTextFlags_ReadOnly);
 
-            auto &fragPath = renderCp.fragPath;
+            auto &fragPath = render.fragPath;
             if (ImGui::Button("Select##Frag"))
             {
                 if (FileDialog::SelectSingleFile(nullptr, nullptr, path))
                 {
                     fragPath = path;
-                    renderCp.reloadShader = true;
+                    render.reloadShader = true;
                 }
             }
             ImGui::SameLine();
-            ImGui::InputText("##FragmentShaderPath", const_cast<char *>(fragPath.c_str()),
-                             fragPath.size(), ImGuiInputTextFlags_ReadOnly);
+            ImGui::InputText("##FragmentShaderPath", &fragPath, ImGuiInputTextFlags_ReadOnly);
         }
     }
+}
+
+void ComponentEditPanel::onAttach(const Ref<Scene> &scene)
+{
+    SceneTargetPanel::onAttach(scene);
+    m_componentSelectionPanel.onAttach(scene);
 }
 
 void ComponentEditPanel::onImGuiRender()
@@ -132,6 +128,5 @@ void ComponentEditPanel::onImGuiRender()
 
     ImGui::EndChild();
 }
-
 
 } // end of namespace rl
