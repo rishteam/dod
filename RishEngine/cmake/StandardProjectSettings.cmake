4ccf1995807bbe41223877ae231569baab787259
roy4801 <a82611141@gmail.com>
2020-10-24 18:32:04 +0800

    [Editor] Small change on cmake and add helps and abouts

--- RishEngine/cmake/StandardProjectSettings.cmake
+++ RishEngine/cmake/StandardProjectSettings.cmake
@@ -10,13 +10,13 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
                                                "MinSizeRel" "RelWithDebInfo")
 endif()
 
-# find_program(CCACHE ccache)
-# if(CCACHE)
-#   message("using ccache")
-#   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-# else()
-#   message("ccache not found cannot use")
-# endif()
+ find_program(CCACHE ccache)
+ if(CCACHE)
+   message("using ccache")
+   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+ else()
+   message("ccache not found cannot use")
+ endif()
 
 # Generate compile_commands.json to make it easier to work with clang based
 # tools
@@ -24,7 +24,7 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 option(ENABLE_IPO
        "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)"
-       OFF)
+       ON)
 if(ENABLE_IPO)
   include(CheckIPOSupported)
   check_ipo_supported(RESULT result OUTPUT output)
