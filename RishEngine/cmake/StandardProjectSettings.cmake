b7fd7a89e8ed096ea7b46aef2a8e7b979150b302
roy4801 <a82611141@gmail.com>
2020-10-26 03:44:21 +0800

    General fix

--- RishEngine/cmake/StandardProjectSettings.cmake
+++ RishEngine/cmake/StandardProjectSettings.cmake
@@ -24,7 +24,7 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 option(ENABLE_IPO
        "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)"
-       ON)
+       OFF)
 if(ENABLE_IPO)
   include(CheckIPOSupported)
   check_ipo_supported(RESULT result OUTPUT output)
