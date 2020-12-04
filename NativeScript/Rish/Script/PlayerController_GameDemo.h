e816dafa1f45218242fd1214fbb8429e07b41a47
ICEJJ <lpc0503@gmail.com>
2020-12-04 18:38:05 +0800

    [Game_Demo] TODO, Add player script

---
+++ NativeScript/Rish/Script/PlayerController_GameDemo.h
@@ -0,0 +1,69 @@
+#include <Rish/Script/Common.h>
+#include <Rish/Physics/Component.h>
+#include <Rish/Script/Script.h>
+
+#include <Rish/rlpch.h>
+#include <Rish/Core/Time.h>
+#include <Rish/Input/Input.h>
+#include <Rish/Math/Random.h>
+
+namespace rl {
+
+    class PlayerController_GameDemo : public ScriptableEntity
+    {
+    public:
+
+        enum class PlayerFace
+        {
+            Left,
+            Right
+        };
+
+        enum class PlayerAction
+        {
+            Stand = 0,
+            Ducking,
+            Jump
+        };
+
+        PlayerAction playerAction = PlayerAction::Stand;
+        PlayerFace playerFace = PlayerFace::Right;
+        PlayerState playerState = PlayerState::Small;
+
+        void onCreate() override;
+
+        void onDestroy() override;
+
+        void onUpdate(Time dt) override;
+
+        void onImGuiRender() override;
+
+        void detectObject();
+
+        void setSprite(SpriteRenderComponent &render, PlayerAction &playerAction, PlayerFace &playerFace);
+
+        RL_SCRIPT_SERIALIZE()
+        {
+            RL_SERIALIZE("walkAccel", walkAccel);
+            RL_SERIALIZE("walkSpeedLimit", walkSpeedLimit);
+            RL_SERIALIZE("jumpSpeed", jumpSpeed);
+        }
+    private:
+
+        float StopSpeed = 0.3;
+
+        // Walk Speed
+        float walkAccel = 5.0f;
+        float walkSpeedLimit = 5.0f;
+
+        // Limit Jump Times
+        bool prevJump = false;
+        float jumpSpeed = 7.0f;
+        int jumpLimitTimes = 1;
+        int jumpTimesCounter = 0;
+
+    };
+
+}
+
+RL_REGISTER_SCRIPT_TYPE(rl::PlayerController_GameDemo) // NOLINT
